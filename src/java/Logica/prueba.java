472a716730784f11d94303ba133ecce1cd4ed342
Jairo Bautista <j.bautista.m13@outlook.com>
2014-10-21 18:18:42 -0500

    segundo commint, haciendo metodos para cada consulta.

--- src/java/Logica/prueba.java
+++ src/java/Logica/prueba.java
@@ -10,6 +10,8 @@
     import java.io.IOException;
     import java.io.UnsupportedEncodingException;
     import java.net.URL;
+    import java.util.logging.Level;
+    import java.util.logging.Logger;
     import org.apache.commons.codec.binary.Base64;
     import org.apache.http.HttpEntity;
     import org.apache.http.HttpResponse;
@@ -24,24 +26,87 @@
     * @author estudiante
     */
     public class prueba {
-        static String url = "http://localhost/index.php?r=admin/remotecontrol";
-	static URL serverURL;
-
+        
+        //------------------------------
+        //Atributos
+        //------------------------------
+        
+        private HttpPost post;
+        private String url;
+	private URL serverURL;
+        private HttpClient cliente;
+        private String key;
+        
+        //-----------------------------
+        //Constructor
+        //-----------------------------
+        
+        public prueba(){
+            post = new HttpPost("http://localhost/index.php?r=admin/remotecontrol");
+            post.setHeader("content-type", "application/json");
+            url = "http://localhost/index.php?r=admin/remotecontrol";
+            try {
+                key = getSessionKey();
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(prueba.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        //----------------------------
+        //Metodos utiles para el app
+        //----------------------------
+        
 	public static String parse(String jsonLine) {
-		JsonElement jelement = new JsonParser().parse(jsonLine);
-		JsonObject jobject = jelement.getAsJsonObject();
-		String result = jobject.get("result").getAsString();
-		return result;
+            JsonElement jelement = new JsonParser().parse(jsonLine);
+            JsonObject jobject = jelement.getAsJsonObject();
+            String result = jobject.get("result").getAsString();
+            return result;
 	}
-
-	public static void main(String[] args) throws UnsupportedEncodingException {
-		HttpClient client = HttpClients.createDefault();
-
+        
+        public String getSessionKey() throws UnsupportedEncodingException{
+            post.setEntity( new StringEntity("{\"method\": \"get_session_key\", \"params\": {\"username\": \"admin\", \"password\": \"password\" }, \"id\": 1}"));
+            try {
+                HttpResponse response = cliente.execute(post);
+                if(response.getStatusLine().getStatusCode() == 200){
+                    HttpEntity entidad = response.getEntity();
+                    key = parse(EntityUtils.toString(entidad));
+                }
+            } catch (Exception e) {
+                System.err.println("error para conseguir el key");
+            }
+            return key;
+        }
+        
+        public String listSurveys(int surveyId) throws IOException{
+            JsonObject request = new JsonObject();
+            JsonObject params = new JsonObject();
+            params.addProperty("method", "list_surveys");
+            request.addProperty("sUser", "admin");
+            System.out.println("Request: " + request.toString());
+            try {
+                post.setEntity(new StringEntity(request.toString()));
+                HttpResponse response = cliente.execute(post);
+                if (response.getStatusLine().getStatusCode() == 200) {
+                    HttpEntity entity = response.getEntity();
+                    entity = response.getEntity();
+                    JsonParser gson = new JsonParser();
+                    JsonObject respuesta = (JsonObject) gson.parse(EntityUtils.toString(entity));
+                    String ans = respuesta.get("result").getAsString();
+                    String stringFromBase = new String(Base64.decodeBase64(ans));
+                    System.out.println("reponse: " + stringFromBase);
+		}
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(prueba.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            return null;
+        }
+        
+        public static String ExportarRespuestas() throws UnsupportedEncodingException{
+            HttpClient client = HttpClients.createDefault();
 		HttpPost post = new HttpPost(
 				"http://localhost/index.php?r=admin/remotecontrol");
 		post.setHeader("Content-type", "application/json");
-		post.setEntity(new StringEntity(
-				"{\"method\": \"get_session_key\", \"params\": {\"username\": \"admin\", \"password\": \"password\" }, \"id\": 1}"));
 		System.out.println(post);
 		try {
 			HttpResponse response = client.execute(post);
@@ -54,7 +119,7 @@
 				request.addProperty("id", 1);
 				request.addProperty("method", "export_responses");
 				params.addProperty("sSessionKey", sessionKey.toString());
-				params.addProperty("iSurveyID", 436586);
+				params.addProperty("iSurveyID", 68124);
 				params.addProperty("sDocumentType", "json");
 				request.add("params", params);
 				System.out.println("Request: " + request.toString());
@@ -82,5 +147,6 @@
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+                return null;
+        }
     }
